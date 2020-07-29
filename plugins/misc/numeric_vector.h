f438897b8f7ff1c1690835f393a4835e157d05bd
Afiq Anuar <afiqaize@gmail.com>
2020-07-30 00:09:50 +0200

    split out multivector_system into main and impl for use in masked functions, and add functions for pt and rapidity ala mass

--- plugins/misc/numeric_vector.h
+++ plugins/misc/numeric_vector.h
@@ -46,6 +46,7 @@ Number dR(Number eta1, Number phi1, Number eta2, Number phi2)
 
 
 
+/// helper for multivector_system_impl, doing the actual daughter assignment
 template <size_t ...N, typename Number>
 void assign_vector(std::array<TLorentzVector, sizeof...(N)> &p4, std::array<Number, 4 * sizeof...(N)> &arg, std::index_sequence<N...>)
 {
@@ -54,8 +55,10 @@ void assign_vector(std::array<TLorentzVector, sizeof...(N)> &p4, std::array<Numb
 
 
 
+/// provide a reference to sum of N 4-momenta
+/// with arg checking to minimize the reassignment i.e. reuse the p4 as much as possible
 template <size_t ...N, typename ...Numbers>
-const TLorentzVector& multivector_system(std::index_sequence<N...>, Numbers ...numbers)
+const TLorentzVector& multivector_system_impl(std::index_sequence<N...>, Numbers ...numbers)
 {
   static_assert(sizeof...(N) > 0 and sizeof...(numbers) == sizeof...(N) and sizeof...(N) % 4 == 0, 
                 "ERROR: multivector_system: arguments are interpreted as pt, eta, phi and mass of "
@@ -82,15 +85,27 @@ const TLorentzVector& multivector_system(std::index_sequence<N...>, Numbers ...n
 
 
 
+/// whenever such a p4 sum is desired, without having to deal with details
+/// for use mainly in the Aggregate attributes that require index masking
+template <typename ...Numbers>
+const TLorentzVector& multivector_system(Numbers ...numbers)
+{
+  return multivector_system_impl(std::make_index_sequence<sizeof...(numbers)>{}, numbers...);
+}
+
+
+
+/// implementation of function returning invariant mass of a p4
 template <typename Number, typename ...Numbers>
 Number invariant_mass_impl(Number number, Numbers ...numbers) 
 { 
-  const auto &p4 = multivector_system(std::make_index_sequence<sizeof...(numbers) + 1>{}, number, numbers...);
+  const auto &p4 = multivector_system(number, numbers...);
   return p4.M();
 }
 
 
 
+/// helper that returns a function pointer to invariant_mass_impl
 template <typename Number, size_t ...N>
 auto invariant_mass_helper(std::index_sequence<N...>) -> Number(*)(typename std::tuple_element<N, std::array<Number, sizeof...(N)>>::type...)
 {
@@ -99,10 +114,63 @@ auto invariant_mass_helper(std::index_sequence<N...>) -> Number(*)(typename std:
 
 
 
+/// a function returning a function that calculates the invariant mass of system consisting of N 4-momenta 
 template <size_t N = 2, typename Number = float>
 auto invariant_mass() -> decltype(invariant_mass_helper<Number>(std::make_index_sequence<4 * N>{}))
 {
   return invariant_mass_helper<Number>(std::make_index_sequence<4 * N>{});
+}
+
+
+
+/// works similarly as invariant_mass_* above, but returns the transverse momentum instead
+template <typename Number, typename ...Numbers>
+Number system_pt_impl(Number number, Numbers ...numbers) 
+{ 
+  const auto &p4 = multivector_system(number, numbers...);
+  return p4.Pt();
+}
+
+
+
+template <typename Number, size_t ...N>
+auto system_pt_helper(std::index_sequence<N...>) -> Number(*)(typename std::tuple_element<N, std::array<Number, sizeof...(N)>>::type...)
+{
+  return system_pt_impl<typename std::tuple_element<N, std::array<Number, sizeof...(N)>>::type...>;
+}
+
+
+
+template <size_t N = 2, typename Number = float>
+auto system_pt() -> decltype(system_pt_helper<Number>(std::make_index_sequence<4 * N>{}))
+{
+  return system_pt_helper<Number>(std::make_index_sequence<4 * N>{});
+}
+
+
+
+/// works similarly as invariant_mass_* above, but returns the rapidity instead
+template <typename Number, typename ...Numbers>
+Number system_rapidity_impl(Number number, Numbers ...numbers) 
+{ 
+  const auto &p4 = multivector_system(number, numbers...);
+  return p4.Rapidity();
+}
+
+
+
+template <typename Number, size_t ...N>
+auto system_rapidity_helper(std::index_sequence<N...>) -> Number(*)(typename std::tuple_element<N, std::array<Number, sizeof...(N)>>::type...)
+{
+  return system_rapidity_impl<typename std::tuple_element<N, std::array<Number, sizeof...(N)>>::type...>;
+}
+
+
+
+template <size_t N = 2, typename Number = float>
+auto system_rapidity() -> decltype(system_rapidity_helper<Number>(std::make_index_sequence<4 * N>{}))
+{
+  return system_rapidity_helper<Number>(std::make_index_sequence<4 * N>{});
 }
 
 #endif
